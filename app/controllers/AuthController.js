d7873a3e8d85955e7775bbaab40bcd5eb7521415
Lucky7Tb <Luckytri101@gmail.com>
2021-03-17 23:58:44 +0700

    fix typo, add email in models

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -2,7 +2,7 @@ const model = require('../models/');
 const operation = model.Sequelize.Op;
 
 async function index(req, res, next) {
-	const data = model.User.finAll();
+	const data = model.User.findAll();
 	res.send(data);
 }
 
